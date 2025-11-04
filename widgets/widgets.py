d1380a29468b08cfe5157aa1344881d4aad326f3
Chad McDaniel <christopher.c.mcdaniel@usm.edu>
2025-11-04 16:10:42 -0600

    Added back-end bindings To Do Item so we can control the title and completed text, as well as the completed indicator's color.

--- widgets/widgets.py
+++ widgets/widgets.py
@@ -1,4 +1,10 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
+from kivy.properties import ColorProperty
+from kivy.utils import get_color_from_hex as hex
 
 class ToDoItemView(BoxLayout):
-    pass
+    title = StringProperty("<title_placeholder>")
+    completed = StringProperty("<completed_placeholder>")    
+    completed_ind_color = ColorProperty(hex("#DD9292"))
+    
