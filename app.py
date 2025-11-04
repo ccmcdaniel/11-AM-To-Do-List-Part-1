6384f19bcc8fb2ab1bd02ee98029611ac5c7876d
Chad McDaniel <christopher.c.mcdaniel@usm.edu>
2025-11-04 14:38:35 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import kivy
+from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
+from views.views import *
+from widgets.widgets import *
+
+Builder.load_file("views/master_list_view.kv")
+Builder.load_file("views/add_new_task_view.kv")
+Builder.load_file("views/edit_task_view.kv")
+Builder.load_file("views/delete_warning_view.kv")
+Builder.load_file("widgets/ToDoItemView.kv")
+Builder.load_file("screen_manager.kv")
+Builder.load_file("style.kv")
+
+class ToDoScreenManager(ScreenManager):
+    pass
+
+class ToDoApp(App):
+    def build(self):
+        return ToDoScreenManager()
+
+if __name__ == "__main__":
+    app = ToDoApp()
+    app.run()
---
+++ views/views.py
@@ -0,0 +1,13 @@
+from kivy.uix.screenmanager import Screen
+
+class MasterListView(Screen):
+    pass
+
+class AddNewTaskView(Screen):
+    pass
+
+class EditTaskView(Screen):
+    pass
+
+class DeleteWarningView(Screen):
+    pass
