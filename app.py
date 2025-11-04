aca701f0f8e4f791a97f29cd71211e68effd4e72
Chad McDaniel <christopher.c.mcdaniel@usm.edu>
2025-11-04 15:26:07 -0600

    Completed the style specification for the app.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import kivy
+from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
@@ -13,6 +14,9 @@ Builder.load_file("views/delete_warning_view.kv")
 Builder.load_file("widgets/ToDoItemView.kv")
 Builder.load_file("screen_manager.kv")
 Builder.load_file("style.kv")
+
+window_scale = 1
+Window.size = (412 * window_scale, 917 * window_scale)
 
 class ToDoScreenManager(ScreenManager):
     pass
